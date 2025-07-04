db1701ff670ac3653087b77b10abe144a3c0e790
Pedro Paulo <pedropaulo727mqs@gmail.com>
2025-07-04 17:12:01 -0300

    bannerHome

---
+++ src/components/banner/BannerHome.jsx
@@ -0,0 +1,13 @@
+import bannerHome from "../../../public/assets/images/bannerHome2.webp";
+
+export default function BannerHome() {
+  return (
+    <div className="w-full my-2 lg:my-10 ">
+      <img
+        src={bannerHome}
+        alt="Banner Home"
+        className="w-full h-full object-cover"
+      />
+    </div>
+  );
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,9 @@
+import BannerHome from "../../components/banner/BannerHome"
+
 export default function Home(){
     return(
         <div>
+            <BannerHome/>
             <h1>Compre aqui no Home tela</h1>
         </div>
     )
