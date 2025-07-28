a53890bc81064ce7f92dce19461e8e32932b89df
Pedro Paulo <pedropaulo727mqs@gmail.com>
2025-07-28 13:22:38 -0300

    shopcart in mobile

--- src/components/navbar/NavbarMobile.jsx
+++ src/components/navbar/NavbarMobile.jsx
@@ -1,5 +1,7 @@
 import { useState, useRef, useEffect } from "react";
-import { CiShoppingCart, CiUser, CiLogout, CiMenuBurger  } from "react-icons/ci";
+import { CiShoppingCart, CiUser, CiLogout, CiMenuBurger } from "react-icons/ci";
+import { IoMdClose } from "react-icons/io";
+
 import Sidebar from "../sidebar/Sidebar";
 import { perfilLinks } from "../../constants/pageNavigationLinks";
 import { Link } from "react-router";
@@ -8,10 +10,17 @@ export default function NavbarMobile() {
     const [sidebarOpen, setSidebarOpen] = useState(false);
 
     const [openDropdownPerfil, setOpenDropdownPerfil] = useState(false);
+    const [openModalShoppingCart, setOpenModalShoppingCart] = useState(null)
+
     const dropdownPerfilRef = useRef(null);
+    const modalShoppingCartRef = useRef(null);
 
     const togglePerfilDropdown = () => {
         setOpenDropdownPerfil((prev) => !prev);
+    };
+
+    const toggleModalShoppingCart = () => {
+        setOpenModalShoppingCart((prev) => !prev);
     };
 
     // Fecha os dropdowns ao clicar fora
@@ -23,6 +32,12 @@ export default function NavbarMobile() {
             ) {
                 setOpenDropdownPerfil(false);
             }
+
+            if (
+                modalShoppingCartRef.current && !modalShoppingCartRef.current.contains(event.target)
+            ) {
+                setOpenModalShoppingCart(false);
+            }
         };
 
         document.addEventListener("mousedown", handleClickOutside);
@@ -32,6 +47,8 @@ export default function NavbarMobile() {
     // Fecha os dropdowns ao mudar de rota
     useEffect(() => {
         setOpenDropdownPerfil(false);
+        setOpenModalShoppingCart(false);
+
     }, [location.pathname]);
 
     return (
@@ -42,7 +59,7 @@ export default function NavbarMobile() {
                 </h1>
 
                 <div className="flex justify-between items-center w-full px-6">
-                    <CiMenuBurger  size={20} className="cursor-pointer" onClick={() => setSidebarOpen(true)} />
+                    <CiMenuBurger size={20} className="cursor-pointer" onClick={() => setSidebarOpen(true)} />
 
                     <ul className="flex gap-3 items-center">
                         <li className="relative" ref={dropdownPerfilRef}>
@@ -65,11 +82,67 @@ export default function NavbarMobile() {
                             )}
 
                         </li>
+
+                        {/* Carrinho */}
                         <li className="relative cursor-pointer">
-                            <CiShoppingCart size={20} />
-                            <div className="absolute -bottom-2 -right-2 flex justify-center items-center rounded-full bg-black text-white w-[15px] h-[15px] text-xs">
-                                <p>48</p>
+                            <div onClick={toggleModalShoppingCart}>
+                                <CiShoppingCart size={20}  />
+                                <div className="absolute -bottom-1 -right-1 flex justify-center items-center rounded-full bg-black text-white w-[15px] h-[15px] text-xs">
+                                    48
+                                </div>
                             </div>
+                            {openModalShoppingCart && (
+                                <ul className="[font-family:'Jost',sans-serif] absolute right-0 top-full mt-2 w-72 bg-white shadow-md border rounded-md z-50 overflow-hidden">
+
+                                    {/* HEADER */}
+                                    <li className="flex justify-between items-center bg-white  font-semibold px-4 py-3">
+                                        <p> Itens Selecionados</p>
+                                        <IoMdClose className="hover:bg-red-900 hover:text-white rounded-full" onClick={toggleModalShoppingCart} />
+                                    </li>
+
+                                    <div className="max-h-74 overflow-y-auto custom-scroll">
+                                        {[...Array(3)].map((_, i) => (
+                                            <li key={i} className="px-4 py-4 bg-white border-b">
+                                                <div className="flex gap-4">
+                                                    {/* Imagem */}
+                                                    <img
+                                                        className="rounded-xl w-24 h-24 object-cover"
+                                                        src="../../../public/assets/images/bolsaPreta.png"
+                                                        alt=""
+                                                    />
+
+                                                    {/* Conteúdo texto + botões */}
+                                                    <div className="flex flex-col justify-between w-full">
+                                                        {/* Texto com limite de linhas */}
+                                                        <div className="text-sm text-gray-700 max-h-20 overflow-hidden">
+                                                            <p className="line-clamp-2">Bolsa Feminina couro preta.</p>
+                                                            <p className="line-clamp-1">Cor: Preto</p>
+                                                            <p className="line-clamp-1 font-medium">R$: 923.32</p>
+                                                        </div>
+
+                                                        {/* Botões de quantidade */}
+                                                        <div className="flex gap-2 justify-start mt-2">
+                                                            <button className="bg-red-900 w-6 h-6 text-white rounded-full hover:bg-red-800 text-sm">+</button>
+                                                            <span className="bg-red-900 w-6 h-6 text-white rounded-full text-center flex items-center justify-center text-sm">01</span>
+                                                            <button className="bg-red-900 w-6 h-6 text-white rounded-full hover:bg-red-800 text-sm">-</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        ))}
+                                    </div>
+
+                                    {/* FOOTER */}
+                                    <li className="px-4 py-3 bg-white font-semibold">
+                                        <p className="text-sm">Total de Item(s): 10</p>
+                                        <p>Valor Total: R$ 499.671,31</p>
+                                        <button className="bg-red-900 text-white w-full p-2 rounded-md mt-2 hover:bg-red-800 hover:cursor-pointer">
+                                            Finalizar Comprar
+                                        </button>
+                                    </li>
+                                </ul>
+                            )}
+
                         </li>
                     </ul>
                 </div>
